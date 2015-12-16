17e2867a3fe00e0131ef0623cb0b7366cbb96b50
Uncle Cheese <aaron@silverstripe.com>
2015-12-16 14:58:31 +1300

    initial commit

---
+++ code/BlogSortableCategories.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Updates the GridField to add the orderable rows component
+ *
+ * @author  Aaron Carlino <aaron@silverstripe.com>
+ * @package  unclecheese/blog-sortable-categories
+ */
+class BlogSortableCategories extends DataExtension {
+
+	/**
+	 * @param  FieldList $fields	 
+	 */
+	public function updateCMSFields (FieldList $fields) {
+		if($grid = $fields->dataFieldByName('Categories')) {
+			$grid->getConfig()->addComponent(new GridFieldOrderableRows());
+		}
+	}
+}
---
+++ code/SortableBlogCategory.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Adds a Sort column to the BlogCategory object, with a default sort
+ *
+ * @author  Aaron Carlino <aaron@silverstripe.com>
+ * @package  unclecheese/blog-sortable-categories
+ */
+class SortableBlogCategory extends DataExtension {
+
+	/**
+	 * @var array
+	 */
+	private static $db = array (
+		'Sort' => 'Int'
+	);
+
+	/**	 
+	 * @var string
+	 */
+	private static $default_sort = 'Sort ASC';
+}
